57dde5cec79a1495fe96781d5974e4338cb95a39
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-03-29 11:20:16 +0200

    drugi sposob

---
+++ lab6/4_2_1a2/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void foo(int n, int *tab)
+{
+    for(int i=0;i<n;i++)
+    {
+        *(tab+i) = 0;
+    }
+}
+
+void wyswietlTablice(int n, int * tab)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("[%d]=%d\n",i, *(tab+i));
+    }
+}
+
+int main()
+{
+    int *tab = malloc(3*sizeof(int));
+    *tab = 3;
+    *(tab+1) = 4;
+    *(tab+2) = 5;
+    wyswietlTablice(3,tab);
+    foo(3, tab);
+    wyswietlTablice(3,tab);
+    return 0;
+}
