b5827393bb034e816219c96d69baf0368e7178fc
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-03-29 11:09:11 +0200

    4_2_1a

---
+++ lab6/4_2_1a/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void foo(int n, int tab[])
+{
+    for(int i=0;i<n;i++)
+    {
+        tab[i] = 0;
+    }
+}
+
+void wyswietlTablice(int n, int tab[])
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("[%d]=%d\n",i, tab[i]);
+    }
+}
+
+int main()
+{
+    int tab[] = {3,4,5};
+    int tab2[] = {-3,2,3,9,11};
+    foo(3, tab);
+    wyswietlTablice(3,tab);
+    foo(5, tab2);
+    wyswietlTablice(5,tab2);
+    return 0;
+}
